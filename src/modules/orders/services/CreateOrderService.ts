4d94d1cbac62471dd445ad7c2d6150bcb1136684
carloseduardoporto <69756514+carloseduardoporto@users.noreply.github.com>
2021-01-25 16:28:20 -0300

    todos testes passaram

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -20,13 +20,78 @@ interface IRequest {
 @injectable()
 class CreateOrderService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customerExists = await this.customersRepository.findById(customer_id);
+
+    if (!customerExists) {
+      throw new AppError('Could not find any customer with given id');
+    }
+
+    const existentProduts = await this.productsRepository.findAllById(products);
+
+    if (!existentProduts.length) {
+      throw new AppError('Could not find any products with given ids');
+    }
+
+    const existentProdutsIds = existentProduts.map(product => product.id); // retorna um array dos ids que existem
+
+    const checkInexistentProducts = products.filter(
+      // verifica os productos que NÃO EXISTEM
+      product => !existentProdutsIds.includes(product.id),
+    );
+
+    if (checkInexistentProducts.length) {
+      // caso haja produto que não existe. Ele lança um erro com o id do produto
+      throw new AppError(
+        `Could not find product ${checkInexistentProducts[0].id}`,
+      );
+    }
+
+    const findProductsWithNoQuantitiesAvailable = products.filter(
+      product =>
+        existentProduts.filter(p => p.id === product.id)[0].quantity <
+        product.quantity,
+    );
+
+    if (findProductsWithNoQuantitiesAvailable.length) {
+      throw new AppError(
+        `The quantity ${findProductsWithNoQuantitiesAvailable[0].quantity} is not available for ${findProductsWithNoQuantitiesAvailable[0].id}`,
+      );
+    }
+
+    const serializedProducts = products.map(product => ({
+      product_id: product.id,
+      quantity: product.quantity,
+      price: existentProduts.filter(p => p.id === product.id)[0].price,
+    }));
+
+    const order = await this.ordersRepository.create({
+      customer: customerExists,
+      products: serializedProducts,
+    });
+
+    const { order_products } = order;
+
+    const orderedProductsQuantity = order_products.map(product => ({
+      id: product.product_id,
+      quantity:
+        existentProduts.filter(p => p.id === product.product_id)[0].quantity -
+        product.quantity,
+    }));
+
+    await this.productsRepository.updateQuantity(orderedProductsQuantity);
+
+    return order;
   }
 }
 
--- src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/infra/typeorm/repositories/ProductsRepository.ts
@@ -21,21 +21,41 @@ class ProductsRepository implements IProductsRepository {
     price,
     quantity,
   }: ICreateProductDTO): Promise<Product> {
-    // TODO
+    const product = await this.ormRepository.create({
+      name,
+      price,
+      quantity,
+    });
+
+    await this.ormRepository.save(product);
+
+    return product;
   }
 
   public async findByName(name: string): Promise<Product | undefined> {
-    // TODO
+    const product = await this.ormRepository.findOne({ where: { name } });
+
+    return product;
   }
 
   public async findAllById(products: IFindProducts[]): Promise<Product[]> {
-    // TODO
+    const productsIds = products.map(product => product.id);
+
+    // retorna os ids de todos os product passador por parâmetro, [2, 3, 6, 8, 1, 2]
+
+    const existentProducts = await this.ormRepository.find({
+      where: {
+        id: In(productsIds), // todos que tem algum número id igual ao do vetor lá em cima
+      },
+    });
+
+    return existentProducts;
   }
 
   public async updateQuantity(
     products: IUpdateProductsQuantityDTO[],
   ): Promise<Product[]> {
-    // TODO
+    return this.ormRepository.save(products);
   }
 }
 
